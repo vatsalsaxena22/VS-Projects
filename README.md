a7e5787b65e42e14606981dee9eb13ac46d54c03
Vatsal Saxena <vatsalsaxena22@gmail.com>
2024-09-13 11:19:12 +0530

    Update README

---
+++ README.md
@@ -0,0 +1,9 @@
+# This is Vatsal Saxena
+## A Full Stack Developer
+### This is my Projects Website
+
+> You can see all my projects here.
+
+Technologies used in my projects includes `HTML`, `CSS`, `JavaScript`, `React.js` and `M.E.R.N. Stack`.
+
+You can visit my [Portfolio](https://vatsalsaxena22.github.io/myPortfolio) also.
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <main className="text-light text-center pt-5 pb-5 ps-3 pe-3">
       <h1 className="display-2 pt-5">This is Vatsal Saxena,</h1>
-      <h1 className="display-6">A MERN Stack Developer....</h1>
+      <h1 className="display-6">A Full Stack Developer....</h1>
       <h1 className="display-1 pb-5">Welcome to my Projects Website!</h1>
     </main>
   );
